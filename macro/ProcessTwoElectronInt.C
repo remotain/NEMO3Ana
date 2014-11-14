2ff0559e99dd3be35cace0261ef08694c0b71778
Alberto Remoto <remoto@ccage011.in2p3.fr>
2014-11-14 10:49:17 +0100

    Processing macro @ ccin2p3

--- macro/ProcessTwoElectronInt.C
+++ macro/ProcessTwoElectronInt.C
@@ -1,17 +1,17 @@
 {
-		
-	gSystem->Load("build/lib/libNEMO3Ana.dylib");
-	gROOT->ProcessLine(".x macro/LoadAllDataSample.C+");
+
+	gSystem->Load("/afs/in2p3.fr/throng/nemo/users/remoto/work/NEMO3Ana/build/lib/libNEMO3Ana.so");
+	gROOT->ProcessLine(".x /afs/in2p3.fr/throng/nemo/users/remoto/work/NEMO3Ana/macro/LoadAllDataSample.C+");
 	
 	// Set Channel to process
 	ProcessChannel::SetChannelToProcess(ProcessChannel::kTwoElectronInternal);
 
 	// Set basic path	
-	ProcessChannel::SetInputFilePath ("/Users/alberto/Software/SuperNEMO/work/nemo3/");
+	ProcessChannel::SetInputFilePath ("/sps/nemo/scratch/remoto/nemo3/data/TwoElectronNtuples_Int/");
 	ProcessChannel::SetInputFileName ("TwoElectronNtuplizerExe_Int_output.root");
 	ProcessChannel::SetInputDirName  ("TwoElectronNtuplizer");
 	ProcessChannel::SetInputTreeName ("TwoElectronNtuplizer");	
-	ProcessChannel::SetOutputFilePath("/Users/alberto/Software/SuperNEMO/work/nemo3/plot/");
+	ProcessChannel::SetOutputFilePath("/sps/nemo/scratch/remoto/nemo3/plot/");
 	ProcessChannel::SetOutputFileName("TwoElectronIntHistos.root");
 		
 	// Run
