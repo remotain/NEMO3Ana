618f4806976f1f0259832dc31d86790eb343c0dc
remotain <remoto@in2p3.fr>
2014-11-30 16:23:53 +0100

    Add clollection for the new interfaces (Parameter,Observable,Component)

--- src/DataManagement.cc
+++ src/DataManagement.cc
@@ -4,8 +4,22 @@ namespace DataManagement{
 
 	// Data Set collection
 	THashList * _DataSetCollection = new THashList();
-	void AddDataSet( DataSet * d){ _DataSetCollection->Add(d); };
+	void AddDataSet( DataSet * tmp){ _DataSetCollection->Add(tmp); };
     THashList * GetDataSetCollection() { return _DataSetCollection; };
 
+	// Observable collection
+	THashList * _ObservableCollection = new THashList();
+	void AddObservable( Observable * tmp){ _ObservableCollection->Add(tmp); };
+    THashList * GetObservableCollection() { return _ObservableCollection; };
+
+	// Component collection
+	THashList * _ComponentCollection = new THashList();
+	void AddComponent( Component * tmp){ _ComponentCollection->Add(tmp); };
+    THashList * GetComponentCollection() { return _ComponentCollection; };
+
+	// Parameter collection
+	THashList * _ParameterCollection = new THashList();
+	void AddParameter( Parameter * tmp){ _ParameterCollection->Add(tmp); };
+    THashList * GetParameterCollection() { return _ParameterCollection; };
 
 }
--- src/DataManagement.h
+++ src/DataManagement.h
@@ -1,11 +1,22 @@
 
 #include "THashList.h"
 #include "DataSet.h"
+#include "Observable.h"
+#include "Component.h"
+#include "Parameter.h"
 
 namespace DataManagement {
 
-	void AddDataSet( DataSet * d);
+	void AddDataSet( DataSet * tmp);
 	THashList * GetDataSetCollection();
 	
-
+	void AddObservable( Observable * tmp);
+	THashList * GetObservableCollection();
+	
+	void AddParameter( Parameter * tmp);
+	THashList * GetParameterCollection();
+	
+	void AddComponent( Component * tmp);
+	THashList * GetComponentCollection();
+	
 };
