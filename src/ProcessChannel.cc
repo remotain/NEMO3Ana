ec11696ecddf6ed1a53638d849fe0d858dc41ec8
remotain <remoto@in2p3.fr>
2015-11-25 10:48:33 +0100

    Restate charge cut

--- src/ProcessChannel.cc
+++ src/ProcessChannel.cc
@@ -623,7 +623,7 @@ namespace ProcessChannel {
 	    cutNames->push_back("All events ");
 		cutNames->push_back("Run status = 1 (GOOD RUN) ");
 		cutNames->push_back("Cd-116 sector (18) ");
-		//cutNames->push_back("Negative track sign");
+		cutNames->push_back("Negative track sign");
 	    cutNames->push_back("Energy of the electron > 300 keV ");
 		cutNames->push_back("Track lengths > 30 cm ");
 		cutNames->push_back("Track fires gg hit in first (0) layers of cells");
@@ -1105,7 +1105,7 @@ namespace ProcessChannel {
 		    if ( !CheckRunNumber(run) )                                         continue;
 			if ( !CheckRunStatus(runType) )                                     continue; hAnaCutFlow -> Fill(currentcut++); // Check the run status
 			if (sectorId != 18 || IsExcludedSpot(el_vtx_z_mean_, vertexSector)) continue; hAnaCutFlow -> Fill(currentcut++);
-		    //if (el_trkSign[0] >=0 or el_trkSign[1] >=0 )					    continue; hAnaCutFlow -> Fill(currentcut++);
+		    if (el_trkSign[0] >=0 or el_trkSign[1] >=0 )					    continue; hAnaCutFlow -> Fill(currentcut++);
 			if (el_energy_[0] < 0.3 or el_energy_[1] < 0.3 )       			    continue; hAnaCutFlow -> Fill(currentcut++);
 		    if (el_pathLength_[0] < 30 or el_pathLength_[1] < 30)        	    continue; hAnaCutFlow -> Fill(currentcut++);
 			if ( el_first_hit_layer_[0] > 0 or el_first_hit_layer_[1] > 0 )     continue; hAnaCutFlow -> Fill(currentcut++);
