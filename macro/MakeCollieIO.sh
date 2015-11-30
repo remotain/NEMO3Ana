cff572b585f038e62ea70b51b46c06bf7aa7ef11
remotain <remoto@in2p3.fr>
2015-11-30 13:39:36 +0100

    Add otion to run RHC eta term

--- macro/MakeCollieIO.sh
+++ macro/MakeCollieIO.sh
@@ -1,12 +1,14 @@
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_BDT MM
-${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_BDT RHC
+${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_BDT RHC_L
+${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_BDT RHC_E
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_BDT M1
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_BDT M2
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_BDT M3
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_BDT M7
 
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_En MM
-${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_En RHC
+${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_En RHC_L
+${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_En RHC_E
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_En M1
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_En M2
 ${THRONG_DIR}/users/remoto/work/NEMO3Ana/build/bin/collieIO_En M3
---
+++ macro/TMVATrainALL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /Users/alberto/Software/ROOT/root.git/bin/thisroot.sh
+
+root -b -q /Users/alberto/Software/SuperNEMO/work/nemo3/NEMO3Ana/macro/TMVAClassification.C"( \"\" , \"MM\"    )";
+root -b -q /Users/alberto/Software/SuperNEMO/work/nemo3/NEMO3Ana/macro/TMVAClassification.C"( \"\" , \"RHC_L\" )";
+root -b -q /Users/alberto/Software/SuperNEMO/work/nemo3/NEMO3Ana/macro/TMVAClassification.C"( \"\" , \"RHC_E\" )";
+root -b -q /Users/alberto/Software/SuperNEMO/work/nemo3/NEMO3Ana/macro/TMVAClassification.C"( \"\" , \"M1\"    )";
+root -b -q /Users/alberto/Software/SuperNEMO/work/nemo3/NEMO3Ana/macro/TMVAClassification.C"( \"\" , \"M2\"    )";
+root -b -q /Users/alberto/Software/SuperNEMO/work/nemo3/NEMO3Ana/macro/TMVAClassification.C"( \"\" , \"M3\"    )";
+root -b -q /Users/alberto/Software/SuperNEMO/work/nemo3/NEMO3Ana/macro/TMVAClassification.C"( \"\" , \"M7\"    )";
+
