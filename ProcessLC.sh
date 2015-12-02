ebc5f5219dcf771f6f8eb4c5de26a94824adc749
remotain <remoto@in2p3.fr>
2015-12-02 17:43:19 +0100

    Bug fix

--- ProcessLC.sh
+++ ProcessLC.sh
@@ -15,8 +15,6 @@ source /usr/local/root/v5.34.18/bin/thisroot.sh
 PATH_EXE=/afs/in2p3.fr/throng/nemo/users/remoto/work/NEMO3Ana/build/bin
 PATH_OUT=/sps/nemo/scratch/remoto/nemo3/plot/plot_UPDATE_TECHNOTE_20151118/Collie
 
-#${SGE_TASK_ID}
-
 if [ "${SGE_TASK_ID}" -eq "1" ]; then ${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_MM.root     ${PATH_OUT}/CollieIO_En_MM.root     ; fi
 if [ "${SGE_TASK_ID}" -eq "2" ]; then ${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_RHC_L.root  ${PATH_OUT}/CollieIO_En_RHC_L.root  ; fi
 if [ "${SGE_TASK_ID}" -eq "3" ]; then ${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_RHC_E.root  ${PATH_OUT}/CollieIO_En_RHC_E.root  ; fi
