8bc332429da947089f24eb4d65834320a95c93ed
remotain <remoto@in2p3.fr>
2015-12-02 17:40:58 +0100

    Try to process as job array

--- ProcessLC.sh
+++ ProcessLC.sh
@@ -8,16 +8,18 @@
 #$ -V
 #$ -j y
 #$ -o /sps/nemo/scratch/remoto/nemo3/plot/
+#$ -t 1-6
 
 source /usr/local/root/v5.34.18/bin/thisroot.sh
 
 PATH_EXE=/afs/in2p3.fr/throng/nemo/users/remoto/work/NEMO3Ana/build/bin
 PATH_OUT=/sps/nemo/scratch/remoto/nemo3/plot/plot_UPDATE_TECHNOTE_20151118/Collie
 
-./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_MM.root    ${PATH_OUT}/CollieIO_En_MM.root 
-./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_RHC_L.root ${PATH_OUT}/CollieIO_En_RHC_L.root 
-./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_RHC_E.root ${PATH_OUT}/CollieIO_En_RHC_E.root 
+#${SGE_TASK_ID}
 
-./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_BDT_MM.root    ${PATH_OUT}/CollieIO_BDT_MM.root 
-./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_BDT_RHC_L.root ${PATH_OUT}/CollieIO_BDT_RHC_L.root 
-./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_BDT_RHC_E.root ${PATH_OUT}/CollieIO_BDT_RHC_E.root 
+if [ "${SGE_TASK_ID}" -eq "1" ]; then ./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_MM.root     ${PATH_OUT}/CollieIO_En_MM.root     ; fi
+if [ "${SGE_TASK_ID}" -eq "2" ]; then ./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_RHC_L.root  ${PATH_OUT}/CollieIO_En_RHC_L.root  ; fi
+if [ "${SGE_TASK_ID}" -eq "3" ]; then ./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_En_RHC_E.root  ${PATH_OUT}/CollieIO_En_RHC_E.root  ; fi
+if [ "${SGE_TASK_ID}" -eq "4" ]; then ./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_BDT_MM.root    ${PATH_OUT}/CollieIO_BDT_MM.root    ; fi
+if [ "${SGE_TASK_ID}" -eq "5" ]; then ./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_BDT_RHC_L.root ${PATH_OUT}/CollieIO_BDT_RHC_L.root ; fi 
+if [ "${SGE_TASK_ID}" -eq "6" ]; then ./${PATH_EXE}/collieLC ${PATH_OUT}/CollieCL_BDT_RHC_E.root ${PATH_OUT}/CollieIO_BDT_RHC_E.root ; fi 
