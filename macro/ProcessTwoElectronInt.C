aaf24fcaa908a71e4359814282b0d66e2a04809a
remotain <remoto@in2p3.fr>
2016-03-10 15:34:43 +0100

    Add files deleted by mistake

---
+++ macro/ProcessTwoElectronInt.C
@@ -0,0 +1,23 @@
+{
+
+	gSystem->Load("/afs/in2p3.fr/throng/nemo/users/remoto/work/NEMO3Ana/build/lib/libNEMO3Ana.so");
+	gROOT->ProcessLine(".x /afs/in2p3.fr/throng/nemo/users/remoto/work/NEMO3Ana/macro/LoadAllDataSample.C+");
+	
+	// Set Channel to process
+	ProcessChannel::SetChannelToProcess(ProcessChannel::kTwoElectronInternal);
+
+	// Set basic path	
+	ProcessChannel::SetInputFilePath ("/sps/nemo/scratch/remoto/nemo3/data/TwoElectronNtuples_Int/");
+	ProcessChannel::SetInputFileName ("TwoElectronNtuplizerExe_Int_output.root");
+	ProcessChannel::SetInputDirName  ("TwoElectronNtuplizer");
+	ProcessChannel::SetInputTreeName ("TwoElectronNtuplizer");	
+	ProcessChannel::SetOutputFilePath("/sps/nemo/scratch/remoto/nemo3/plot/");
+	ProcessChannel::SetOutputFileName("TwoElectronIntHistos.root");
+		
+	// Run
+	TBenchmark * benchmark = new TBenchmark();
+	benchmark->Start("Run");
+	ProcessChannel::Run();
+	benchmark->Show("Run");
+	
+}
